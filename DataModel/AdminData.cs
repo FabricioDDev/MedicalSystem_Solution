8706f4b7dbf167c8c3e8079dbdab52cb44cb1745
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2022-12-08 18:18:24 -0300

    AdmData-ListSP-Completed

--- DataModel/AdminData.cs
+++ DataModel/AdminData.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using DB;
 
 namespace DataModel
 {
-    internal class AdminData
+    public class AdminData
     {
+        public AdminData() { data = new DataAccess(); }
+        private DataAccess data;
+        
+        public List<Admin> ListSP()
+        {
+            List<Admin> list = new List<Admin>();
+            try
+            {
+                data.Query("AdminList");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    Admin aux = new Admin();
+                    aux.Id = (int)data.PropReader["Id"];
+                    aux.Email = (string)data.PropReader["Email"];
+                    aux.PropPassword = (string)data.PropReader["Pass"];
+                    list.Add(aux);
+                }
+                return list;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { data.Close(); }
+            
+        }
     }
 }
