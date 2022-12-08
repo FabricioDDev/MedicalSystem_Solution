729d18d44964c21f96a9014570a54c7824007a42
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2022-12-08 09:00:10 -0300

    Admin-Completed

---
+++ DomainModel/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        
+        private string Password { get; set; }
+        public string PropPassword
+        {
+            get { return Password; }
+            set { Password = value; }
+        }
+    }
+}
