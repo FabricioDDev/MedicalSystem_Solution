5a295628a1ca9cbed144f4fe3eac22d5a34e7d40
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2022-12-10 18:57:44 -0300

    AppointmentList-Function-completed

--- DataModel/AppointmentData.cs
+++ DataModel/AppointmentData.cs
@@ -13,6 +13,42 @@ namespace DataModel
         public AppointmentData() { data = new DataAccess(); }
         private DataAccess data;
 
+        public List<Appointment> AppointmentListSP()
+        {
+            List<Appointment> list = new List<Appointment>();
+            try
+            {
+                data.SP("AppointmentList");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    Appointment aux = new Appointment();
+                    aux.Id = (int)data.PropReader["Id"];
+                    aux.Time = (string)data.PropReader["Time"];
+                    aux.date = (DateTime)data.PropReader["AppointmentDate"];
 
+                    aux.medical = new Medical();
+                    aux.medical.Id = (int)data.PropReader["Mid"];
+                    aux.medical.FullName = (string)data.PropReader["Mname"];
+
+                    aux.patient = new Patient();
+                    aux.patient.Id = (int)data.PropReader["Pid"];
+                    aux.patient.FullName = (string)data.PropReader["Pname"];
+
+                    aux.state = new State();
+                    aux.state.Id = (int)data.PropReader["Sid"];
+                    aux.state.Name = (string)data.PropReader["Name"];
+
+                    aux.query = new Query();
+                    aux.query.Id = (int)data.PropReader["id"];
+                    aux.query.Name = (string)data.PropReader["name"];
+
+                    list.Add(aux);
+                }
+                return list;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { data.Close(); }
+        }
     }
 }
--- DomainModel/Appointment.cs
+++ DomainModel/Appointment.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
 
 namespace DomainModel
 {
-    internal class Appointment
+    public class Appointment
     {
+        public int Id { get; set; }
+        public string Time { get; set; }
+        public DateTime date { get; set; }
+        public Medical medical { get; set; }
+        public Patient patient { get; set; }
+        public State state { get; set; }
+        public Query query { get; set; }
     }
 }
