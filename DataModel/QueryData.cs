2463f06bb59243afb8d206275b3687e2495d9bfe
Fabricio Di Paola <fabriciodamiandipaola@gmail.com>
2022-12-11 09:56:12 -0300

    QueryList

--- DataModel/QueryData.cs
+++ DataModel/QueryData.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using DB;
 
 namespace DataModel
 {
-    internal class QueryData
+    public class QueryData
     {
+        public QueryData() { data = new DataAccess(); }
+        private DataAccess data;
+
+        public List<Query> QueryList()
+        {
+            List<Query> list = new List<Query>();
+            try
+            {
+                data.Query("Select Id, Name from QueryTypes");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    Query aux = new Query();
+                    aux.Id = (int)data.PropReader["Id"];
+                    aux.Name = (string)data.PropReader["Name"];
+                    list.Add(aux);
+                }
+                return list;
+            }
+            catch (Exception ex) { throw ex; }
+            finally { data.Close(); }
+        }
     }
 }
